04c113693bfb4bfe73f7f8539777aa0a7f98969c
Deni247 <deniseseti7@gmail.com>
2025-08-19 17:32:06 +0200

    Displayed Announcements

--- src/pages/Annoucements.jsx
+++ src/pages/Annoucements.jsx
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Annoucements = () => {
-
+const [announcements, setAnnouncements] = useState([]);
 const fetchAnnouncements = async () => {
   try {
     const response = await fetch('https://pcm-backend.vercel.app/api/announcements', {
@@ -13,6 +13,7 @@ const fetchAnnouncements = async () => {
     });
 
     const data = await response.json();
+    setAnnouncements(data);
     console.log("response", data);
   } catch (error) {
     console.error("Error fetching announcements:", error);
@@ -28,6 +29,12 @@ const fetchAnnouncements = async () => {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-4">Announcements</h1>
       <p className="text-lg">This page is under construction.</p>
+      {announcements.map((announcement)=> (
+       <div key={announcement._id} className="bg-gray-100 p-4 rounded-lg shadow-md mb-4">
+        title: {announcement.title}
+        message: {announcement.message}
+       </div> 
+      ))}
     </div>
   );
 }
